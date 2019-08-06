8b7b7ccc88164d003547a52a2d3a640e9ab7b2a5
nagaohiroki <testplayers0226@gmail.com>
2019-08-06 22:33:33 +0900

    change engine

--- MyProject/Source/MyProject/MyUserWidgetTest.cpp
+++ MyProject/Source/MyProject/MyUserWidgetTest.cpp
@@ -1,6 +1,11 @@
 #include "MyUserWidgetTest.h"
+#include "Engine.h"
 
 void UMyUserWidgetTest::NativeTick(const FGeometry& MyGeometry, float InDeltaTime) 
 {
 	Super::NativeTick(MyGeometry, InDeltaTime);
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 0.0f, FColor::Red, TEXT(""));
+	}
 }
